7b0929d4992eea073f2909cac51d66a45cc10a11
kjdeepak <kjdeepak4work@gmail.com>
2022-03-20 12:08:30 +0530

    Create module file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     NgbModule
   ],
   providers: [],
---
+++ src/app/technology/technology.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TechnologyRoutingModule } from './technology-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TechnologyRoutingModule
+  ]
+})
+export class TechnologyModule { }
